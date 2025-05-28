890710b36ba0ac3c266a32039e4f9cf0e70627cd
minye.li <yueyuepapa@gmail.com>
2025-05-28 09:56:49 +0800

    update

--- flutter_fimber/android/src/main/kotlin/com/magillus/flutterfimber/FlutterFimberPlugin.kt
+++ flutter_fimber/android/src/main/kotlin/com/magillus/flutterfimber/FlutterFimberPlugin.kt
@@ -6,7 +6,6 @@ import io.flutter.plugin.common.MethodCall
 import io.flutter.plugin.common.MethodChannel
 import io.flutter.plugin.common.MethodChannel.MethodCallHandler
 import io.flutter.plugin.common.MethodChannel.Result
-import io.flutter.plugin.common.PluginRegistry.Registrar
 
 class FlutterFimberPlugin :FlutterPlugin, MethodCallHandler {
     /// The MethodChannel that will the communication between Flutter and native Android
